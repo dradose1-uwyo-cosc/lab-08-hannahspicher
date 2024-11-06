376c8a68650552ecb52fe8d985e997bbf0c45d5a
hannahspicher <hspicher@uwyo.edu>
2024-11-06 01:23:01 +0000

    finish

--- cosc1010-lab08.py
+++ cosc1010-lab08.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Hannah Spicher
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section:
+# 11/05/24
+# Lab 08
+# Lab Section: 11
 # Sources, people worked with, help given to:
 # your
 # comments
@@ -13,6 +13,24 @@
 # If they can't be converted return false
 # Other wise return the converted int or float 
 # Floats should only have one decimal point in them 
+def tester(string):
+    try:
+        integer = int(string)
+        return integer
+    except ValueError:
+        pass
+    try:
+        if string.count('.') == 1:
+            float_number = float(string)
+            return float_number
+    except ValueError:
+        pass
+
+    return False
+print(tester("12.3"))
+print(tester("12.0"))
+print(tester("1.2.1"))
+print(tester("2..9"))
 
 
 print("*" * 75)
@@ -32,11 +50,47 @@ print("*" * 75)
 # Check to make sure that the lower bound is less than or equal to the upper bound
 # m, b can be floats or integers
 # the bounds must be integers, if not return false
+def slope_intercept (m,b,a,an):
+
+    m_number = tester(m)
+    b_number = tester(b)
+    an_number = tester(an)
+    a_number = tester(a)
+    if a_number > an_number:
+        return False
+    if type(a_number)==float or type(an_number) == float:
+        return False
+    if ((m_number or m_number == 0) and (b_number or b_number ==0) and (a_number or a_number == 0) and (an_number or an_number == 0)):
+        answer=[]
+        for x in range(a_number, an_number+1):
+            y=m_number*x+b_number
+            answer.append(y)
+        return answer
+    
 
 # Create a while loop to prompt users for their input for the four variables
 # Exit on the word exit
 # Remember all inputs are strings, but the function needs ints or floats
 # Call your function and print the resulting list
+while True:
+    m = input("Enter the slope for your equation (b) or type 'exit' to quit:")
+    if m.lower() == 'exit':
+        break
+    b = input("Enter the y-intercept for your equation (b) or type 'exit' to quit:")
+    if b.lower() == 'exit':
+        break
+    a = input("Enter lower bound here (a) or type 'exit' to quit:")
+    if a.lower() == 'exit':
+        break
+    an = input("Enter the upper bound here (an) or type 'exit' to quit:")
+    if an.lower() == 'exit':
+        break
+    final_answer = slope_intercept(m,b,a,an)
+    if final_answer is not False:
+            print("The y values in the range:",final_answer)
+    else:
+            print("Error, try again")
+            
 
 print("*" * 75)
 
@@ -48,3 +102,36 @@ print("*" * 75)
 # Create a loop like above to prompt the user for input for the three values
 # Create a second function that just does the square root operation 
     # If the number you are trying to take the square root of is negative, return null
+def quadratic_formula(a,b, c):
+    if (sqrt(a,b,c) == False):
+         return False
+    x1= (-b + sqrt(a,b,c))/2*a
+    
+    x2 = (-b - sqrt(a,b,c))/2*a
+    
+    return x1, x2
+
+def sqrt(a,b,c):
+    y= (b**2)-(4*a*c)
+    if y < 0:
+        return False
+    else:
+        return y**(1/2)
+while True:
+    a = (input("Enter A value or type 'exit' to quit:"))
+    if a.lower() == 'exit':
+        break
+    b = (input("Enter B value or type 'exit' to quit:"))
+    if b.lower() == 'exit':
+        break
+    c = (input("Enter C value or type 'exit' to quit:"))
+    if c.lower() == 'exit':
+        break
+    a=float(a)
+    b=float(b)
+    c=float(c)
+    quad_answer = quadratic_formula(a,b,c)
+    if quad_answer is not False:
+            print("Answer:",quad_answer)
+    else:
+            print("Error, try again")
